09a089553db3e651f13345158502cbf9331b912e
Paul Mozo <paul.mozo@datasift.com>
2018-05-22 22:15:26 +0100

    Update ReturnClause.php

--- src/Clauses/ReturnClause.php
+++ src/Clauses/ReturnClause.php
@@ -5,19 +5,19 @@ class ReturnClause extends Clause{
 
 	protected $clauseName = 'RETURN';
 
-    public function return($variable, $attribute = ''){
+    	public function return($variable, $attribute = ''){
 
-    	$append = !empty($this->getClause());
+    		$append = !empty($this->getClause());
 
-    	if ($append){
-    		$this->addToClause(', ');
+    		if ($append){
+    			$this->addToClause(', ');
+    		}
+
+        	if (empty($attribute)){
+            		$this->addToClause($variable);
+        	}
+        	else {
+        		$this->addToClause($variable.'.'.$attribute);   
+        	}
     	}
-
-        if (empty($attribute)){
-            $this->addToClause($variable);
-        }
-        else {
-        	$this->addToClause($variable.'.'.$attribute);   
-        }
-    }
 }
