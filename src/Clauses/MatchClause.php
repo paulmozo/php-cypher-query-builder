12f525ed8e531184cfe150db4b02658572a997f0
Paul Mozo <paul.mozo@datasift.com>
2018-05-22 22:14:39 +0100

    Update MatchClause.php

--- src/Clauses/MatchClause.php
+++ src/Clauses/MatchClause.php
@@ -8,28 +8,28 @@ class MatchClause extends Clause{
 
 	protected $relationShipMatch = false;
 
-    public function match($label, $variable = ''){
-        if (!$this->relationShipMatch){
-        	$this->nodeMatch($label, $variable);
-        }
-        else {
-        	$this->relationshipMatch($label, $variable);	
-        }
-        $this->relationShipMatch = !$this->relationShipMatch;
-    }
-
-    private function nodeMatch($label, $variable){
-    	$this->addToClause("($variable:$label)");
-    }
-
-    private function relationshipMatch($label, $variable){
-    	$this->addToClause("-[$variable:$label]-");
-    }
-
-    public function getClause(){
-    	if (!$this->relationShipMatch){
-    		throw new MatchEndingWithRelationshipException('Match clauses cannot end with a relationship match. ');
+	public function match($label, $variable = ''){
+		if (!$this->relationShipMatch){
+			$this->nodeMatch($label, $variable);
+        	}
+        	else {
+        		$this->relationshipMatch($label, $variable);	
+        	}
+        	$this->relationShipMatch = !$this->relationShipMatch;
     	}
-        return $this->clause;
-    }
+
+    	private function nodeMatch($label, $variable){
+    		$this->addToClause("($variable:$label)");
+    	}
+
+    	private function relationshipMatch($label, $variable){
+    		$this->addToClause("-[$variable:$label]-");
+    	}
+
+    	public function getClause(){
+    		if (!$this->relationShipMatch){
+    			throw new MatchEndingWithRelationshipException('Match clauses cannot end with a relationship match. ');
+    		}
+        	return $this->clause;
+    	}
 }
