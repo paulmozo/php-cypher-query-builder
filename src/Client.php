407adaf4309e949b6c0d4fcaf657aaf3837a32c1
Paul Mozo <paul.mozo@autodata-group.com>
2018-05-30 20:16:57 +0100

    Got dynamic method generation working, just need to tidy it up

--- src/Client.php
+++ src/Client.php
@@ -17,16 +17,12 @@ class Client{
 
 	private $returnClause;
 
+	private $generatedMethods;
+
 	private $clauses = [];
 
 	public function __construct(){
 		$this->reset();
-
-		$this->clauses = [
-			$this->matchClause,
-			$this->whereClause,
-			$this->returnClause
-		];
 	}
 
 	public function match($label, $variable = ''){
@@ -59,5 +55,45 @@ class Client{
 		$this->matchClause = new MatchClause;
 		$this->whereClause = new WhereClause;
 		$this->returnClause = new ReturnClause;
+
+		$this->clauses = [
+			$this->matchClause,
+			$this->whereClause,
+			$this->returnClause
+		];
+
+		$this->createAppendMethod($this->matchClause);
+		$this->createAppendMethod($this->whereClause);
+		$this->createAppendMethod($this->returnClause);
+
 	}
+
+	/**
+	 * Creates a custom append method for each clause at run time
+	 */
+	private function createCustomAppendMethods(){
+		foreach ($this->clauses as &$clause){
+			var_dump($clause);
+			$this->createAppendMethod($clause);
+		}
+	}
+
+	private function createAppendMethod($clause){
+		$funcClause = strtolower($clause->getClauseName()).'Clause';
+		$appendFunc = function ($string) use ($funcClause) {
+			$this->$funcClause->addToClause($string);
+			return $this;
+		};
+
+		$funcName = 'appendCustom'.ucwords(strtolower($clause->getClauseName()));
+
+		$this->generatedMethods[$funcName] = \Closure::bind($appendFunc, $this, get_class());
+	}
+
+	function __call($method, $args) {
+          if(is_callable($this->generatedMethods[$method]))
+          {
+            return call_user_func_array($this->generatedMethods[$method], $args);
+          }
+     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,22 +5,46 @@ use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase{  
 
-  public function testMatchSingleNode(){
-	$client = new Moozla\QueryBuilder\Client();
-	$client->match('Person');
-	$this->assertEquals('MATCH (:Person)', (string)$client);
-  }
+	public function testMatchSingleNode(){
+		$client = new Moozla\QueryBuilder\Client();
+		$client->match('Person');
+		$this->assertEquals('MATCH (:Person)', (string)$client);
+	}
 
-  public function testComplexQuery(){
-    $client = new Moozla\QueryBuilder\Client();
+	public function testComplexQuery(){
+		$client = new Moozla\QueryBuilder\Client();
 
-    $client
-      ->match('Person', 'person')
-      ->match('LIKES')
-      ->match('Movie', 'movie')
-      ->where('movie', 'name', "=", 'Taxi Driver')
-      ->return('movie');
+		$client
+		->match('Person', 'person')
+		->match('LIKES')
+		->match('Movie', 'movie')
+		->where('movie', 'name', "=", 'Taxi Driver')
+		->return('movie');
 
-    $this->assertEquals('MATCH (person:Person)-[:LIKES]-(movie:Movie) WHERE movie.name = "Taxi Driver" RETURN movie', (string)$client);
-  }
+		$this->assertEquals('MATCH (person:Person)-[:LIKES]-(movie:Movie) WHERE movie.name = "Taxi Driver" RETURN movie', (string)$client);
+	}
+
+	public function testCustomMatch(){
+		$client = new Moozla\QueryBuilder\Client();
+
+		$client
+		->match('Person', 'person')
+		->match('LIKES')
+		->match('Movie', 'movie')
+		->appendCustomMatch(' Here Is My Custom Cypher');
+
+		$this->assertEquals('MATCH (person:Person)-[:LIKES]-(movie:Movie) Here Is My Custom Cypher', (string)$client);
+	}
+
+	public function testCustomCombination(){
+		$client = new Moozla\QueryBuilder\Client();
+
+		$client
+		->match('Person', 'person')
+		->appendCustomMatch(' Custom match Cypher')
+		->appendCustomWhere('Custom where Cypher')
+		->appendCustomReturn('Custom return Cypher');
+
+		$this->assertEquals('MATCH (person:Person) Custom match Cypher WHERE Custom where Cypher RETURN Custom return Cypher', (string)$client);
+	}
 }
